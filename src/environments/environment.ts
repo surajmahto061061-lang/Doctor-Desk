3a4af7bf693bff6e297043cd2ff992aa705c16dc
Rajankumar15 <Rajankumar15@gmail.com>
2026-08-15 17:05:51 +0530

    Use npx for build script, update production environment URLs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: '/api',
-  wsUrl: 'https://solvixon-healthcare-production.up.railway.app/ws'
+  production: true,
+  apiUrl: 'https://solvixon-healthcare-production.up.railway.app/api',
+  wsUrl: 'wss://solvixon-healthcare-production.up.railway.app/ws'
 };
